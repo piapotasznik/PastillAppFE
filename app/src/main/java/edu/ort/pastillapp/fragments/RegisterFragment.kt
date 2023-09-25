0bf93584edb73158229cfa87eb39ae803cf91f0a
Joa Herreros <joaherreros03@gmail.com>
2023-09-25 15:08:35 -0300

    Desarrollo de pantalla de registro

--- app/src/main/java/edu/ort/pastillapp/fragments/RegisterFragment.kt
+++ app/src/main/java/edu/ort/pastillapp/fragments/RegisterFragment.kt
@@ -1,56 +1,76 @@
 package edu.ort.pastillapp.fragments
 
+import android.content.ContentValues
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.navigation.findNavController
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
 import edu.ort.pastillapp.R
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import edu.ort.pastillapp.UserSingleton
 
-/**
- * A simple [Fragment] subclass.
- * Use the [RegisterFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class RegisterFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    lateinit var v: View
+    val auth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+   ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        v = inflater.inflate(R.layout.fragment_register, container, false)
+        return v
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RegisterFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic fun newInstance(param1: String, param2: String) =
-                RegisterFragment().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
+    override fun onStart() {
+        super.onStart()
+        val singUp = v.findViewById<Button>(R.id.singUpBtn)
+
+        singUp.setOnClickListener{
+            val userName = v.findViewById<EditText>(R.id.singUpName).text.toString();
+            // el text es para obtener el valor del campo, y luego lo parseo a String
+            val userEmail = v.findViewById<EditText>(R.id.singUpemail).text.toString();
+            val passPass = v.findViewById<EditText>(R.id.singUpPassword).text.toString();
+            val passPass2 = v.findViewById<EditText>(R.id.singUpPassword2).text.toString();
+
+
+            if (passPass != passPass2 ){
+                val text = "Password are not the same!"
+                val duration = Toast.LENGTH_SHORT
+
+                //snackbar
+                val snackbar = Snackbar.make(v,text,duration)
+                snackbar.setTextColor(Color.RED)
+                // snackbar.setBackgroundTint(getResources().getColor(R.color.black))
+
+                snackbar.show()
+            } else{
+                auth.createUserWithEmailAndPassword(userEmail, passPass).addOnCompleteListener(){
+                    if (it.isSuccessful){
+                        Log.d(ContentValues.TAG, "createUserWithEmail:success")
+                        val user = auth.currentUser
+                        UserSingleton.currentUser = user
+
+                        val action = RegisterFragmentDirections.actionRegisterFragmentToProfileUserFragment()
+                        v.findNavController().navigate(action)
                     }
                 }
+            }
+
+            }
+
     }
 }
