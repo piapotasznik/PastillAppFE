9f22c8460c26532407208c3c8cdd59a7bc95c5c1
Camila Ingberg <camilaingberg@gmail.com>
2023-10-25 22:52:54 -0300

    notifications fixed

--- app/src/main/java/edu/ort/pastillapp/Services/MyFirebaseMessagingService.kt
+++ app/src/main/java/edu/ort/pastillapp/Services/MyFirebaseMessagingService.kt
@@ -1,29 +1,34 @@
-package edu.ort.pastillapp.Services
+package edu.ort.pastillapp
+
 import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.os.Build
+import androidx.appcompat.app.AlertDialog
 import androidx.core.app.NotificationCompat
-import edu.ort.pastillapp.R
+import edu.ort.pastillapp.Activities.InitActivity
 
-class MyFirebaseMessagingService: FirebaseMessagingService() {
- override fun onMessageReceived(remoteMessage: RemoteMessage) {
-     // Aquí puedes manejar las notificaciones cuando se reciban
-     if (remoteMessage.notification != null) {
-         val title = remoteMessage.notification!!.title
-         val body = remoteMessage.notification!!.body
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        if (remoteMessage.notification != null) {
+            val title = remoteMessage.notification?.title
+            val body = remoteMessage.notification?.body
+            val tipo = remoteMessage.data["tipo"]
+            val reminderLogId = remoteMessage.data["id"]
 
-         // Puedes mostrar la notificación o realizar otras acciones aquí
-         // Por ejemplo, mostrar un log con el título y el cuerpo de la notificación
-         Log.d(TAG, "Título: $title")
-         Log.d(TAG, "Cuerpo: $body")
+            Log.d(TAG, "Título: $title")
+            Log.d(TAG, "Cuerpo: $body")
+            Log.d(TAG, "Tipo: $tipo")
+            Log.d(TAG, "ID del ReminderLog: $reminderLogId")
 
-         mostrarDialogoDeNotificacion(title, body)
-     }
- }
+            mostrarNotificacion(title, body, tipo, reminderLogId)
+        }
+    }
 
     override fun onNewToken(token: String) {
         // Este método se llama cuando se genera un nuevo token o se actualiza el token
@@ -33,7 +38,12 @@ class MyFirebaseMessagingService: FirebaseMessagingService() {
         // Puedes hacer lo que necesites con el token, como enviarlo a tu servidor
     }
 
-    private fun mostrarDialogoDeNotificacion(title: String?, body: String?) {
+    private fun mostrarNotificacion(
+        title: String?,
+        body: String?,
+        tipo: String?,
+        reminderLogId: String?
+    ) {
         val channelId = "canal_de_notificacion_ppal"
         val notificationBuilder = NotificationCompat.Builder(this, channelId)
             .setSmallIcon(R.drawable.logo)
@@ -41,9 +51,9 @@ class MyFirebaseMessagingService: FirebaseMessagingService() {
             .setContentText(body)
             .setPriority(NotificationCompat.PRIORITY_HIGH)
 
-        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
-        // Si deseas, puedes crear un canal de notificación para Android Oreo y versiones posteriores
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val channel = NotificationChannel(
                 channelId,
@@ -54,11 +64,37 @@ class MyFirebaseMessagingService: FirebaseMessagingService() {
         }
         val notificationId = System.currentTimeMillis().toInt()
 
+        val resultIntent = when (tipo) {
+            "alarm" -> {
+                // Configurar la acción para abrir el fragmento de alarma y pasar el ID
+                Intent(this, InitActivity::class.java)
+                    .apply { putExtra("reminderLogId", reminderLogId) }
+            }
+
+            "emergencycontact" -> {
+                // Configurar la acción para abrir el fragmento de contacto de emergencia
+                Intent(this, InitActivity::class.java)
+            }
+
+            else -> {
+                // Configurar la acción predeterminada (puedes ajustarla según tus necesidades)
+                Intent(this, InitActivity::class.java)
+            }
+        }
+
+        val pendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            resultIntent,
+            PendingIntent.FLAG_IMMUTABLE
+            //    PendingIntent.FLAG_UPDATE_CURRENT
+        )
+        notificationBuilder.setContentIntent(pendingIntent)
+        notificationBuilder.setAutoCancel(true)
         notificationManager.notify(notificationId, notificationBuilder.build())
     }
 
     companion object {
         private const val TAG = "MyFirebaseMessagingService"
     }
-
 }
